7cac2c11a1573c4ff35b3e0af8947617df558941
Daria <gdaria1609@yandex.ru>
2023-11-30 23:37:22 +0500

    add

---
+++ Bits.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Bits
+{
+    internal class Bits
+    {
+        public Bits(long b)
+        {
+            this.Value = b;
+        }
+        public Bits(int c)
+        {
+            this.Value2 = c;
+
+        }
+        public Bits(byte d)
+        {
+            this.Value3 = d;
+        }
+
+        public long Value { get; private set; } = 0;
+        public int Value2 { get; private set; } = 0;
+        public int Value3 { get; private set; } = 0;
+
+
+        public static implicit operator Bits(long b) => new Bits(b);
+        public static implicit operator Bits(int c) => new Bits(c);
+        public static implicit operator Bits(byte d) => new Bits(d);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_Bits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long b = 20;
+            var bits = (Bits)b;
+            Console.WriteLine(bits.Value);
+
+            int c = 50;
+            var bits1 = (Bits)c;
+            Console.WriteLine(bits1.Value2);
+
+            byte d= 70;
+            var bits2 = (Bits)d;
+            Console.WriteLine(bits2.Value3);
+        }
+    }
+}
